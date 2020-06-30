b7799740205552e59ed824aaa6b9d85d017720e1
Mathieu Boudreau <emb6150@gmail.com>
2020-06-29 23:48:23 -0300

    Update filenames and filenames nomenclature

--- copy_jsonyaml_to_t1folder.m
+++ copy_jsonyaml_to_t1folder.m
@@ -7,21 +7,21 @@ function copy_jsonyaml_to_t1folder(filepath, name, osfDataset, dataType, dataSub
             srcYamlFile = [osfDataset(1:end-5), filesep, filepath, filesep, name, '.yaml'];
             srcJsonFile = [osfDataset(1:end-5), filesep, filepath, filesep, name,'.json'];
     
-            destYamlFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name(1:end-9), 'Complex_t1map', '.yaml'];
-            destJsonFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name(1:end-9), 'Complex_t1map', '.json'];
+            destYamlFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name(1:end-9), 'Complex_T1map', '.yaml'];
+            destJsonFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name(1:end-9), 'Complex_T1map', '.json'];
         elseif strcmp(dataSubType, 'imaginary')
             srcYamlFile = [osfDataset(1:end-5), filesep, filepath, filesep, name, '.yaml'];
             srcJsonFile = [osfDataset(1:end-5), filesep, filepath, filesep, name,'.json'];
     
-            destYamlFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name(1:end-4), 'Complex_t1map', '.yaml'];
-            destJsonFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name(1:end-4), 'Complex_t1map', '.json'];
+            destYamlFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name(1:end-4), 'Complex_T1map', '.yaml'];
+            destJsonFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name(1:end-4), 'Complex_T1map', '.json'];
         end
     else
         srcYamlFile = [osfDataset(1:end-5), filesep, filepath, filesep, name, '.yaml'];
         srcJsonFile = [osfDataset(1:end-5), filesep, filepath, filesep, name, '.json'];
     
-        destYamlFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name, '_t1map', '.yaml'];
-        destJsonFile  = [osfDataset(1:end-5), '_t1map', filesep, filepath, filesep, name, '_t1map','.json'];
+        destYamlFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name, '_T1map', '.yaml'];
+        destJsonFile  = [osfDataset(1:end-5), '_T1map', filesep, filepath, filesep, name, '_T1map','.json'];
     end
     
     copyfile(srcYamlFile, destYamlFile);
